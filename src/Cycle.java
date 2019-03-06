c82637aaadcce8b012b7998317415b7f050762cb
aditi choudhary <aditichoudhary@aditis-MacBook-Air.local>
2019-03-06 18:05:33 -0500

    finished the reading in of data and tested. Created a matrix of locations called data. The first value is the amount of houses horizantly, the second value is the amount of houses down vertically. It adds 10 * the adress plus the house alphabet value(for A it would add 1 more). git push

--- src/Cycle.java
+++ src/Cycle.java
@@ -1,47 +1,72 @@
-import java.util.Scanner;
+package src;
+
+import java.util.*;
 import java.io.*;
-import java.util.ArrayList;
+
 
 public class Cycle
 {
-    public ArrayList<String[]> setCycle(int x)
+    public static ArrayList<String[]> setCycle(int x)
     {
         ArrayList<String[]> cyc = new ArrayList<String[]>();
-        String line;
-        String[] arr = new String[3];
+        int bart;
+        int lisa;
         try {
-            File file = new File("cycle"+x+".txt");
+            File file = new File("cycledata/cycle"+x+".txt");
             Scanner scanner = new Scanner(file);
-            int i = 0;
-            while (scanner.hasNext()) {
-                if (i>1) {
-                    line = scanner.next();
-                    arr[0] = line.substring(0,line.indexOf("s"));
-                    arr[1] = line.substring(line.indexOf(",")+1,line.indexOf("a"));
-                    arr[2] = line.substring(line.indexOf("a")+2);
+            int count = 1;
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                if (line.equals("Bart Complex")) {
+                    bart = Integer.valueOf(scanner.nextLine());
+                } else if (line.equals("Lisa Complex")) {
+                    lisa = Integer.valueOf(scanner.nextLine());
+                } else if (count > 2) {
+                    String[] arr = line.split(",");
+                    arr[0] = arr[0].substring(0,arr[0].length()-1);
+                    arr[1] = arr[1].substring(0,arr[1].length()-1);
+                    
+                   
+                    //System.out.println(Arrays.toString(arr));
                     cyc.add(arr);
                 }
-                else {
-                    i++;
-                }
+                count++;
+                // arr[0] = line.substring(0,line.find('s'));
+                // arr[1] = line.substring(line.find(',')+1,line.find('a'));
+                // arr[2] = line.substring(line.find('a')+1);
+                
             }
             scanner.close();
+            
+        
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+        
         return cyc;
     }
 
-    public String[] coords(ArrayList<String[]> al)
+    public static Integer[][] coords(ArrayList<String[]> al)
     {
-        Integer[] array = new Integer[al.size()];
-        Integer[] pair = new Integer[2];
-        char c;
+        Integer[][] array = new Integer[al.size()][2];
+        char letter;
+        int house;
         for (int i=0;i<array.length;i++)
         {
-            pair[0] = Integer.parseInt(al.get(i)[0]) * 2;
-            pair[1] = Integer.parseInt(al.get(i)[0]) * 10;
+            letter = al.get(i)[2].charAt(0);
+            house = returnnumberbasedonletter(letter);
+            array[i][0] = Integer.parseInt(al.get(i)[0]) * 2;
+            array[i][1] = Integer.parseInt(al.get(i)[1]) * 10 + house;
             
         }
+        return array;
+    }
+    public static int returnnumberbasedonletter(char letter)
+    {
+        letter = Character.toLowerCase(letter);
+        int temp = (int)letter;
+        int temp_integer = 96; //for lower case
+       
+        return temp-temp_integer;
     }
 }
