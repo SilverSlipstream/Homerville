b06f46c9c91602e2edb92bc2054b64b147a9d287
unknown <aemb011@gmail.com>
2019-03-06 20:16:12 -0500

    Added Algorithm class and finished Cycle class

--- src/src/Cycle.java
+++ src/src/Cycle.java
@@ -17,7 +17,7 @@ public class Cycle
             int count = 1;
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
-                if (line.equals("Bart Complex")) {
+                if (line.equals("Bart Complex")){
                     bart = Integer.valueOf(scanner.nextLine());
                 } else if (line.equals("Lisa Complex")) {
                     lisa = Integer.valueOf(scanner.nextLine());
@@ -26,14 +26,12 @@ public class Cycle
                     arr[0] = arr[0].substring(0,arr[0].length()-1);
                     arr[1] = arr[1].substring(0,arr[1].length()-1);
                     
-                   
-                    //System.out.println(Arrays.toString(arr));
+
                     cyc.add(arr);
                 }
-                count++;
-                // arr[0] = line.substring(0,line.find('s'));
-                // arr[1] = line.substring(line.find(',')+1,line.find('a'));
-                // arr[2] = line.substring(line.find('a')+1);
+                else {
+                    count++;
+                }
                 
             }
             scanner.close();
@@ -54,19 +52,19 @@ public class Cycle
         for (int i=0;i<array.length;i++)
         {
             letter = al.get(i)[2].charAt(0);
-            house = returnnumberbasedonletter(letter);
+            house = numToLetter(letter);
             array[i][0] = Integer.parseInt(al.get(i)[0]) * 2;
             array[i][1] = Integer.parseInt(al.get(i)[1]) * 10 + house;
-            
+
         }
         return array;
     }
-    public static int returnnumberbasedonletter(char letter)
+    public static int numToLetter(char letter)
     {
         letter = Character.toLowerCase(letter);
         int temp = (int)letter;
         int temp_integer = 96; //for lower case
-       
+
         return temp-temp_integer;
     }
 }
